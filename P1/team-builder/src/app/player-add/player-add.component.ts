c1af431b5c90a9d78e2250c27554b242321f8c9d
Daedgutiv <daavogutis@gmail.com>
2019-11-14 10:37:55 +0100

    adding files

---
+++ P1/team-builder/src/app/player-add/player-add.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Player } from '../player';
+import { PlayerService } from '../player.service';
+import { Location } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+
+
+@Component({
+  selector: 'app-player-add',
+  templateUrl: './player-add.component.html',
+  styleUrls: ['./player-add.component.css']
+})
+export class PlayerAddComponent implements OnInit {
+
+  players: Player[];
+  Position = "delantero";
+
+  constructor(private playerService: PlayerService,
+    private location: Location) { }
+
+  ngOnInit() {
+    this.getPlayers();
+  }
+
+  getPlayers(): void {
+    this.playerService.getPlayers().subscribe(players => this.players = players);
+  }
+
+  add(name: string, dorsal: number, position: string): void {
+    name = name.trim();
+    position = this.Position;
+
+    var matches = name.match(/\d+/g);
+
+    if (matches != null || dorsal > 100 || dorsal < 0) {
+      return;
+    }
+    if (isNaN(dorsal)) {
+      return;
+    }
+    this.playerService.addPlayer({ name, dorsal, position } as Player)
+      .subscribe(player => {
+        this.players.push(player);
+      });
+    this.location.back();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
+
